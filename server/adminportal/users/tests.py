58aa3871cb9043811ede256e9cd397a020ef28af
jsen95 <jsen0795@gmail.com>
2019-12-04 00:40:02 -0600

    wrote successful unit tests for users model

--- server/adminportal/users/tests.py
+++ server/adminportal/users/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from .models import CustomUser
+from .forms import CustomUserCreationForm
 
 # Create your tests here.
+class UserModelCreationlTest(TestCase):
+
+  def setUp(self):
+    CustomUser.objects.create(
+      username='test_user',
+      full_name='John Smith',
+      email='johnsmith@yahoo.com',
+    )
+
+  def test_user_fields(self):
+    user = CustomUser.objects.get(id=1)
+    expected_user_name = f'{user.username}'
+    expected_full_name = f'{user.full_name}'
+    expected_email = f'{user.email}'
+    self.assertEqual(expected_user_name, 'test_user')
+    self.assertEqual(expected_full_name, 'John Smith')
+    self.assertEqual(expected_email, 'johnsmith@yahoo.com')
+    
