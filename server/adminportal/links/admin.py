712007841be05b32d712a9c375a8cd4dfb1c906a
jsen95 <jsen0795@gmail.com>
2019-12-05 12:20:37 -0600

    got links associated with each username to display in links section of Django's built-in super admin page

--- server/adminportal/links/admin.py
+++ server/adminportal/links/admin.py
@@ -4,8 +4,8 @@ from .models import Link
 # Register your models here.
 
 class LinkAdmin(admin.ModelAdmin):
-  list_display = ('id','link_address' ,'link_category')
-  list_display_links = ('link_address' ,'link_category')
+  list_display = ('id','link_address' ,'link_category', 'user')
+  list_display_links = ('link_address' ,'link_category', 'user')
 
 admin.site.register(Link, LinkAdmin)
 
